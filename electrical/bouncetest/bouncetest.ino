d07320d96e2b50d5280c250804313f3f3bf5cea6
Daniel Xing <danielxing97@gmail.com>
2019-02-26 19:21:02 -0500

    updated bouncetest to new working parameters

--- electrical/bouncetest/bouncetest.ino
+++ electrical/bouncetest/bouncetest.ino
@@ -14,11 +14,13 @@
  * current set to 1.5A (max 1.7)
  */
 
-#define REVOLUTION 1600
+#define REVOLUTION 6400
 
 #define STEP_PIN 2
 #define DIR_PIN 3
 #define ENA_PIN 4
+
+int x = 0, y = 0;
 // Define a stepper and the pins it will use
 AccelStepper stepper(AccelStepper::DRIVER, STEP_PIN, DIR_PIN); // Defaults to AccelStepper::FULL4WIRE (4 pins) on 2, 3, 4, 5
 void setup()
@@ -26,37 +28,52 @@ void setup()
   // Change these to suit your stepper if you want
   pinMode(ENA_PIN, OUTPUT);
   digitalWrite(ENA_PIN, LOW);
-  stepper.setMaxSpeed(REVOLUTION/4);
-  stepper.setAcceleration(REVOLUTION/2);
+  stepper.setMaxSpeed(REVOLUTION*1.5);
+  stepper.setAcceleration(REVOLUTION);
   stepper.moveTo(REVOLUTION);
+  Serial.begin(115200);
+  delay(5000);
 }
 
 void delayfun(int delaytime) {
-    while(stepper.distanceToGo() != 0)
-      stepper.run();
+    while(stepper.distanceToGo() != 0) {
+      x++;
+      if(x % 1000 == 0) {
+        y=1;
+      }
+        stepper.run();\     
+    }
+
+    stepper.run();
     delay(delaytime);
 }
 
 void loop()
 {
     digitalWrite(ENA_PIN, HIGH);
+    delay(1000);
     // If at the end of travel go to the other end
 //    if (stepper.distanceToGo() == 0)
 //      delay(1000);
 //      stepper.moveTo(REVOLUTION+stepper.currentPosition());
 //    stepper.run();
-  static long pos = 0;
-  for(int i = 0; i < 5; i++) {
-    pos += REVOLUTION;
+  static uint64_t pos = 0;
+//  Serial.println(pos - 1);
+  for(int i = 0; i < 4; i++) {
+    pos += 5*REVOLUTION + REVOLUTION/100;
     stepper.moveTo(pos);
     delayfun(1000);
+//    Serial.println(pos);
+//    Serial.println(sizeof(uint64_t));
   }
 
-  for(int i = 0; i < 5; i++) {
-    pos -= REVOLUTION;
+  for(int i = 0; i < 1; i++) {
+    pos -= 2*REVOLUTION + REVOLUTION/100;
     stepper.moveTo(pos);
     delayfun(1000);
   }
+  Serial.println(y);
+  delay(1000);
 /*
   stepper.moveTo(REVOLUTION/2);
   delayfun(1000);
