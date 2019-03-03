2508e2e377bd23ef5d55db0cb6ff40c3c418c547
mflaher1 <45111766+mflaher1@users.noreply.github.com>
2019-03-03 03:09:36 -0500

    added full functionality. Pending testing of this code, it *might* actually open the safe
    Read long paragraph of comments at the very top of the program. It has some notes on things that should be tested in case the code doesn't initially work. Try these things first. These are mostly library-related things that are likely to cause problems if the code doesn't work. This code has been modified to attempt to crack the safe using the AccelStepper.h library. Note that a kill switch should be added to mitigate stalling after the safe opens!

--- software/autoDialer.ino
+++ software/autoDialer.ino
@@ -1,21 +1,26 @@
 ////////////////////////////////
 
 /// Dial rotation/combination
-
 /// Uses AccelStepper.h as provided in the Electrical folder
-
+/// consider calling disableOutputs from library once safe has been opened. It will shut off power to motor pins and save power.
 ////////////////////////////////
 
 /*Instructions for wire connections:
-
 Connect the Anode of an LED to pin 8, and the cathode to ground. 
-
 Connect the SIG terminal of the photogate to pin 2 (and PWR to 5V, GRND to ground)
-
 Connect the motor's PUL+ to 5V (power); connect PUL- to pin 4, DIR- to pin 3, and ENA- to pin 5.
-
 */
 
+
+/*The goal of the code is as follows: first, call call toZero(), which will keep rotating the motor clockwise until the photogate gets tripped. At this point, the motor will be stopped. Call setCurrentPosition(0), which will set the internal class variable that keeps track of the motor's position back to zero.
+ * As a consequence of this function call, it sets the current speed to 0. I'm not sure if that will cause problems when we call this function every time the photogate gets tripped. We might have to follow that call with a call to a function which changes the current speed back to whatever value it was at when the motor was crossing the photogate (or a value close to it). This remains to be tested. 
+ * Now, begin executing the triple for loop, starting with 0,0,0. Each time the photogate is interrupted, call currentPosition(), which returns the current position in microsteps, relative to the 0 position. positive values are clockwise past 0. Check if the currentPosition is greater than REVOLUTION/100 steps away from zero. If so, that means that the motor thought it was more than a digit off. If that's the case, call setCurrentPosition(0) and redo the combination. Otherwise, just call setCurrentPosition(0).
+ * If, for some reason, the motor speed slows down as a result of resetting position to zero after tripping the photogate, we might have to live with only resetting the position to zero every n_th time the photogate gets tripped and hope the accumulated error is small enough. This is something that we need to test.
+ * Also, the convention used in the first program is that positive rotations are in accordance with positive direction on the unit circle (i.e. a positive rotation would be the counterclockwise). The AccelStepper.h library uses the opposite convention. Rather than flip all of the signs in the code that we have already wrriten, try flipping the wiring of the phase A and B between the motor and driver.
+ * If, for some reason, this doesn't work, then go back into the code and flip every sign in the argument to a stepper.move() call. You may need to do more than this.
+ * 
+ */
+ 
 #include <math.h>
 #include <stdio.h>
 //#include "stepper.h" //using AccelStepper.h now to control acceleration and velocity
@@ -34,86 +39,52 @@ void clear_display()
      LCD.write(0x01); 
 }
 
-
 void cursor_to_line_1()
 {
     LCD.write(0xFE); //command flag
-
     LCD.write(128); //position
-
 }
-
-
 
 //Put cursor to beginning of line 2:
 
 void cursor_to_line_2()
-
  {
-
     LCD.write(0xFE); //command flag
-
     LCD.write(192); //position
-
  }
 
-
-
 //Move cursor right by one:
-
 void cursor_right_1()
 
 {
 
     LCD.write(254);
-
     LCD.write(20);
-
 }
 
-
-
 void print_combo(int x,int y,int z)
-
 {
-
   LCD.write("Trying: ");
-
   cursor_right_1();
-
   LCD.print(x,DEC);
-
   LCD.print(" ");
-
   LCD.print(y,DEC);
-
   LCD.print(" ");
-
   LCD.print(z,DEC);
-
   LCD.write(254);
-
   LCD.write(128);
-
   cursor_to_line_1();
 
 }
 
+
 //#define DEBUG  //put debug code in #ifdef block so we just need to remove this to remove debugging
 
-
-
 #define DBG_DELAY 200 /// debug delay b/w combo digits in tryCombo
-
-
 
 #define NUM_DIGITS 100  /// dial digit count
 
 #define STEP_DIGIT 3    /// dial step (2*tolerance of dial)
-
-
-
-
 
 #define MICROSTEPS 32
 
@@ -127,16 +98,17 @@ void print_combo(int x,int y,int z)
 
 //#define LED_PIN 13  //for LED display
 
-#define REVOLUTION 6400
+#define REVOLUTION 6400  //macro representing the amount of steps/revolution that the motor achieves, which we configured on the driver. It, and STEPS_PER_DIGIT below, are used often as arguments to functions, which often require the argument as a number of steps
+
+#define STEPS_PER_DIGIT 64
+
 
 volatile int32_t pos = 0; //value will be changed in the ISR, so should be declared volatile
-
+volatile int32_t delta = 0;  //value represents the amount of DIGITS to move
 AccelStepper stepper(AccelStepper::DRIVER, STEP_PIN, DIR_PIN);  //object of class AccelStepper using library
-
 bool zeroTrigger = false;
-
-
-
+bool beenTriggeredBefore = false;
+bool tryComboAgain = false;
 //variable below is purely for user debugging
 
 //------------------------------------
@@ -145,31 +117,51 @@ volatile byte ledState = HIGH;
 
 //----------------------------------
 
-
-
-
-
 //should be tied to photogate with Arduino interrupt
 
 //will want trigger to happen on rising edge, not while input is HIGH (infinite trigger)
 
 void onZeroTriggered()
 {
-  stepper.move(0);
-  
-  zeroTrigger = true;
+  if(!beenTriggeredBefore)
+  {
+    stepper.move(0);
+    zeroTrigger = true;
+    
+    //the LED will change value each time photogate is interrupted. can delete two lines of code below after satisfied with results
+    //digitalWrite(LED_PIN, ledState);
+    //ledState = !ledState;
+  }
+  else
+  {
+    if(stepper.currentPosition() > STEPS_PER_DIGIT) //if motor thinks it is more than a single digit away from where it actually is when dial passes 0
+    {
+      tryComboAgain = 1;
+    }
+    
+  }
 
-  pos = 0;
-
-  //the LED will change value each time photogate is interrupted. can delete two lines of code below after satisfied with results
-
-  //digitalWrite(LED_PIN, ledState);
-  //ledState = !ledState;
-
+  stepper.setCurrentPosition(0);
   return;
 }
 
 
+//delta is the number of digits on the dial you wish to rotate. This function will update the global 'pos' variable to reflect where the new position should be after the motor rotates delta digits on the dial.
+static inline void updatePosition(int32_t delta)
+{
+ //pos gets updated to reflect the amount that the dial turns (delta amount) since the last value of pos
+  pos = (pos - delta)%NUM_DIGITS;
+  if( pos < 0 )      //if (pos - delta) is negative, then modulus will return a negative number (in C and Java). We need a positive number. ex: -3 mod 100 will return -3. Need it to return 97.
+  {
+      pos = pos + NUM_DIGITS;
+  }
+  
+  if( (pos >= NUM_DIGITS) || (pos < 0) )
+  {
+      pos = pos % NUM_DIGITS;
+  }
+  return;
+}
 
 void toZero()
 {
@@ -194,110 +186,73 @@ void toZero()
 
 }
 
-
-
 void tryCombo( int32_t c0, int32_t c1, int32_t  c2 )
-
 {
-
 #ifdef DEBUG 
-
   Serial.println("Reset and try combo:");
-
   Serial.println(c0);Serial.println(c1);Serial.println(c2);
-
   delay(DBG_DELAY*2);
-
 #endif
-
-
-
-  toZero();
-
-
 
   //dial combination digits
 
-  
-
 #ifdef DEBUG 
-
   Serial.println("Going to first number...");
-
   delay(DBG_DELAY);
-
 #endif
 
+  delta = (c0 - pos > 0) ? c0 - pos : c0 - pos + NUM_DIGITS;
+  stepper.move( delta*STEPS_PER_DIGIT );
+  updatePosition(delta);
 
-
-  //spin( (c0 - pos > 0) ? c0 - pos : c0 - pos + NUM_DIGITS );
-
-  //spin( 3*NUM_DIGITS ); //stop when dial mark reaches c0 second time
-
-
-
+  delta = 3*NUM_DIGITS;
+  stepper.move( delta*STEPS_PER_DIGIT ); //stop when dial mark reaches c0 second time
+  updatePosition(delta);
+  
 #ifdef DEBUG 
-
   Serial.println("Going to second number");
-
   delay(DBG_DELAY);
-
 #endif
 
-
-
-  //spin( (c1 - pos < 0) ? c1 - pos : c1 - pos - NUM_DIGITS );
-
- // spin( -2*NUM_DIGITS ); //stop when dial mark reaches c1 third time
-
-
-
-#ifdef DEBUG 
-
-  Serial.println("Going to third number");
-
-  delay(DBG_DELAY);
-
-#endif
-
-
-
-  //spin( (c2 - pos > 0) ? c2 - pos : c2 - pos + NUM_DIGITS );
-
-  //spin( NUM_DIGITS ); //stop when dial mark reaches c2 second time
+  delta = (c1 - pos < 0) ? c1 - pos : c1 - pos - NUM_DIGITS;
+  stepper.move( delta*STEPS_PER_DIGIT );
+  updatePosition(delta);
 
   
+  delta = -2*NUM_DIGITS;
+  stepper.move( delta*STEPS_PER_DIGIT ); //stop when dial mark reaches c1 third time
+  updatePosition(delta);
 
-#ifdef DEBUG
-
-  Serial.println("Checking combination...");
-
+#ifdef DEBUG 
+  Serial.println("Going to third number");
   delay(DBG_DELAY);
-
 #endif
 
 
+  delta = (c2 - pos > 0) ? c2 - pos : c2 - pos + NUM_DIGITS;
+  stepper.move( delta*STEPS_PER_DIGIT );
+  updatePosition(delta);
 
-  //TODO: spin once to see if safe opens
+  delta = NUM_DIGITS;
+  stepper.move( delta*STEPS_PER_DIGIT ); //stop when dial mark reaches c2 second time
+  updatePosition(delta);
+  
+#ifdef DEBUG
+  Serial.println("Checking combination...");
+  delay(DBG_DELAY);
+#endif
 
-  //just spin around for now
+  // rotate around once to see if safe opens
 
-  //spin( -NUM_DIGITS );
-
+  delta = -NUM_DIGITS;
+  stepper.move( delta*STEPS_PER_DIGIT );
+  updatePosition(delta);
   return;
-
 }
 
-
-
 void setup()
-
 {
-
-
-
   Serial.begin(9600);
-
   pinMode(INTERRUPT_PIN, INPUT_PULLUP); //make pin 2 an interrupt pin
   pinMode(ENABLE_PIN, OUTPUT);
   pinMode(DIR_PIN, OUTPUT);
@@ -317,53 +272,39 @@ void setup()
 
   delay(500); // wait for display to boot up
 
-digitalWrite(ENABLE_PIN, HIGH);
+  digitalWrite(ENABLE_PIN, HIGH);
 
   clear_display(); 
-
-  
 
   LCD.write("Beginning Safe Cracking Now...");
 
   delay(3000);
-
-
-
  clear_display();
-
-  
-
  cursor_to_line_1();
-
   //code to run once goes here
-
 }
 
-
-
 void loop()
-
 {
-
   toZero();
 
-delay(100000);
+  for( uint32_t x = 0; x < NUM_DIGITS; x += STEP_DIGIT )
 
-//  for( uint32_t x = 0; x < NUM_DIGITS; x += STEP_DIGIT )
-//
-//        for( uint32_t y = 0; y < NUM_DIGITS; y += STEP_DIGIT )
-//
-//          for( uint32_t z = 0; z < NUM_DIGITS; z += STEP_DIGIT )
-//
-//          {
-//
-//                  print_combo(x,y,z);
-//
-//                  tryCombo( x, y, z );
-//
-//                  clear_display();
-      
-//
-//          }
+        for( uint32_t y = 0; y < NUM_DIGITS; y += STEP_DIGIT )
+
+          for( uint32_t z = 0; z < NUM_DIGITS; z += STEP_DIGIT )
+
+          {
+
+                  print_combo(x,y,z);
+
+                  tryCombo( x, y, z );
+
+                  if(tryComboAgain) //if the amount of error between where the motor thinks it is when the dial crosses 0 and where it actually is is greater than a single digit, redo the combo again.
+                  {z -= STEP_DIGIT; tryComboAgain = 0;}
+
+                  clear_display();
+
+          }
 
 }
